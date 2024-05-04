fbd7dab407029cf2b934012e9aa1fd1895f0b4cd
Aditya Kumar <adityapal1409@gmail.com>
2024-05-04 23:18:08 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useSelector } from "react-redux";
 import "./App.css";
+import EditTodo from "./components/EditTodo/EditTodo";
 import TaskInput from "./components/TaskInput/TaskInput";
 import TaskList from "./components/TaskList";
 
 function App() {
+	const { toggleForm } = useSelector((state) => state.todos);
 	return (
 		<div className="App">
 			<h1 style={{ textAlign: "center" }} className="heading">
 				My_Todo_List
 			</h1>
 
-			<TaskInput />
+			{toggleForm ? <TaskInput /> : <EditTodo />}
+
 			<TaskList />
 		</div>
 	);

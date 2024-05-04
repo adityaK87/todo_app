2755d7ae78269e6dd39fd7704e413b71c6299e15
Aditya Kumar <adityapal1409@gmail.com>
2024-05-04 16:16:08 +0530

    render TodoCard on todolist

---
+++ src/components/TaskList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoCard from "./TodoCard/TodoCard";
+const TaskList = () => {
+	return (
+		<div>
+			<TodoCard title="Todo" />
+			<TodoCard title="Todo" />
+			<TodoCard title="Todo" />
+		</div>
+	);
+};
+
+export default TaskList;
